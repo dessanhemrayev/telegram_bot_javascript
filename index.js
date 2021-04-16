66ff5b7c4359d6c91a93e29221f800bd9ab686fa
dessanhemrayev <dessanhemrayev@gmail.com>
2021-04-16 11:50:44 +0300

    telegram_bot

---
+++ index.js
@@ -0,0 +1,69 @@
+const TelegramBot = require('node-telegram-bot-api'); // подключаем node-telegram-bot-api
+
+const token = ''; // тут токен кторый мы получили от botFather
+
+// включаем самого обота
+const bot = new TelegramBot(token, {polling: true});
+
+//конфиг клавиатуры
+const keyboard = [
+    [
+      {
+        text: 'Хочу карту', // текст на кнопке
+        callback_data: 'moreMaps' // данные для обработчика событий
+      }
+    ],
+    [
+        {
+          text: 'Хочу песика',
+          callback_data: 'morePes'
+        }
+    ],
+    [
+        {
+          text: 'Хочу проходить курсы',
+          url: 'https://htmlacademy.ru/continue' //внешняя ссылка
+        }
+      ]
+  ];
+
+// обработчик события присылания нам любого сообщения
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id; //получаем идентификатор диалога, чтобы отвечать именно тому пользователю, который нам что-то прислал
+
+  // отправляем сообщение
+  bot.sendMessage(chatId, 'Привет, Друг! чего хочешь?', { // прикрутим клаву
+        reply_markup: {
+            inline_keyboard: keyboard
+        }
+    });
+});
+
+// обработчик событий нажатий на клавиатуру
+bot.on('callback_query', (query) => {
+    const chatId = query.message.chat.id;
+
+    let img = '';
+
+    if (query.data === 'moreMaps') { // если кот
+        img = 'source/img_1.jpg';
+    }
+
+    if (query.data === 'morePes') { // если пёс
+        img = 'pes.png';
+    }
+
+    if (img) {
+        bot.sendPhoto(chatId, img, { // прикрутим клаву
+            reply_markup: {
+                inline_keyboard: keyboard
+            }
+        });
+    } else {
+        bot.sendMessage(chatId, 'Непонятно, давай попробуем ещё раз?', { // прикрутим клаву
+            reply_markup: {
+                inline_keyboard: keyboard
+            }
+        });
+    }
+  });
